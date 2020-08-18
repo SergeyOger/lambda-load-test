ce727eebc4d654146eb1ca9e17e7f0e35a0f95e5
Sergey <geniuserii85@gmail.com>
2020-08-18 23:04:16 +0300

    updated function code

--- src/main/java/net/broscorp/lambda/EventHandler.java
+++ src/main/java/net/broscorp/lambda/EventHandler.java
@@ -2,6 +2,7 @@ package net.broscorp.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,20 +11,23 @@ public class EventHandler implements RequestHandler<Map<String, Object>, Calcula
   @Override
   public CalculationResult handleRequest(Map<String, Object> input, Context context) {
 
-//    BigInteger argument = BigInteger.valueOf(Long.parseLong(input.getParameter("argument")));
-//
-//    BigInteger result = BigInteger.ONE;
-//
-//    while (!argument.equals(BigInteger.ZERO)) {
-//      result = result.multiply(argument);
-//      argument = argument.subtract(BigInteger.ONE);
-//    }
+    String rawRequestPath = (String) input.get("path");
+    String[] params = rawRequestPath.split("/");
+
+    BigInteger argument = BigInteger.valueOf(Long.parseLong(params[params.length - 1]));
+
+    BigInteger result = BigInteger.ONE;
+
+    while (!argument.equals(BigInteger.ZERO)) {
+      result = result.multiply(argument);
+      argument = argument.subtract(BigInteger.ONE);
+    }
 
     Map<String, String> headers = new HashMap<>();
     headers.put("X-Powered-By", "AWS Lambda, Java11 & serverless");
     headers.put("Content-Type", "application/json");
     return CalculationResult.builder().setStatusCode(200)
-        .setRawBody("Request: " + input.get("argument"))
+        .setRawBody("Request path: " + rawRequestPath + "Calculation result: " + result)
         .setHeaders(headers).build();
   }
 }
